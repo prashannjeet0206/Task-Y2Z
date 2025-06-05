6ec16f06ebfd5f6f7fbcbf03aadabab196c4c005
prashannjeetHexan <prasannjeet.p@hexantech.com>
2025-06-05 10:38:41 +0530

    added title,rating,reviews,description and overflow feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Places from "./Components/Places";
 
 function App() {
   return (
-    <div className="grid grid-cols-2 w-[80%] h-screen mx-auto mt-4 overflow-y-hidden">
+    <div className="grid grid-cols-2 w-[80%] h-screen mx-auto mt-4 overflow-y-hidden shadow-xl rounded-2xl">
       <Itinerary />
       <Places />
     </div>
--- src/Components/ItineraryCard.jsx
+++ src/Components/ItineraryCard.jsx
@@ -1,4 +1,6 @@
 import { Places } from "../Data/Places";
+import { IoReorderThreeOutline } from "react-icons/io5";
+import { MdStarBorder } from "react-icons/md";
 
 const ItineraryCards = () => {
   return (
@@ -14,6 +16,7 @@ const ItineraryCards = () => {
               key={place.id}
               className="flex items-start gap-4 bg-white rounded-xl shadow p-4"
             >
+              {/* Images */}
               <div className="relative">
                 <img
                   src={place.image}
@@ -23,6 +26,22 @@ const ItineraryCards = () => {
                 <span className="bg-purple-600 text-white w-6 h-6 rounded-full text-sm flex items-center justify-center absolute bottom-[50%] -left-3">
                   {index + 1}
                 </span>
+                <span className="absolute bottom-[25%] -left-6">
+                  <IoReorderThreeOutline className="text-2xl" />
+                </span>
+              </div>
+
+              {/* title,rating,description */}
+              <div className="flex-1">
+                <h3 className="font-bold">{place.name}</h3>
+                <p className="flex items-center gap-4 text-gray-400 font-bold">
+                  {place.rating}{" "}
+                  <MdStarBorder className="text-yellow-500 text-xl" />{" "}
+                  {`(${place.reviews.toLocaleString()})`}
+                </p>
+                <p className="bg-slate-100 p-2 rounded-md text-sm">
+                  {place.description}
+                </p>
               </div>
             </div>
           );
